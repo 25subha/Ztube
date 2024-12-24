eca33756b278b7d9ce3506047b69e2ed7bafd30f
subhankar <mrsubhankar25@gmail.com>
2024-12-24 20:03:33 +0530

    create a middelewares uplod on cloudenary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from "fs"
+
+//   Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET,  
+    });
+    
+
+    const uplonOnCloudinary =  async (localFilePath) => {
+        try {
+            if (!localFilePath) return null
+            // uplod the file on cloudinary
+            const response = await cloudinary.uploader.upload(localFilePath, {
+                resource_type: "auto"
+            })
+            //file has been uploded sucess fully 
+            console.log("file is uploded on cloudinery ", response.url)
+            return response;
+        } catch (error) {
+            fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload oparation got failed
+            return null
+        }
+    }
+
+
+    export {uplonOnCloudinary};
+
