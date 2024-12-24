b57921accc972fff0bf30562c3d275d9d9caf894
subhankar <mrsubhankar25@gmail.com>
2024-12-24 20:27:41 +0530

    creae a user router

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,19 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
 
 const ragisterUser = asyncHandler(async (req, res) => {
+    // get user details from frontend
+    // validation - not empty 
+    // check if user already exists: userName, email
+    // check for images, check for avatar.
+    // upload them to cloudinary, avatar check
+    // create user object - create entry in db
+    // removed password or refresh token fild from response
+    // check for user creation (if user null  response or user created sucessfull)
+    // return response
+
+    const { userName, email, fullName, password } = req.body
+    console.log(userName)
+
     res.status(200).json({
         message: "ok"
     })
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
+import { ragisterUser } from "../controllers/user.controller.js";
 
 const router = Router();
 
-
-
+router.route("/register").post(ragisterUser)
 export default router;
